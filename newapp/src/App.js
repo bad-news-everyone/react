aa3bb3feb04b10309711fed490bba99d90cd4a9c
bad-news-everyone <miss.budnik@list.ru>
2020-03-01 17:39:16 +0300

    kk

---
+++ newapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { sum } from './math.js';
+import { App2 } from './math.js';
+
+alert(sum);
+
+export default App;
+/*
+export const App3 = ({ number }) => {
+  return <h1> { number } </h1>
+};*/
