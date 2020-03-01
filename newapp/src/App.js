c80e82ab968aed63e4a32f4bf0584edbc6452007
bad-news-everyone <miss.budnik@list.ru>
2020-03-02 00:34:58 +0300

    kk

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Math } from './math';
-//import './number.js';
 
 export class App3 extends React.Component {
   render() {
     return <div>
       <h1>Hello</h1>
-      <h2>{this.props.name}</h2>
-      <h3>{this.props.secondName}</h3>
+      <h2>{this.props.name} and { Math }</h2>
+      <h3>{ Math }</h3>
     </div>;
   }
 }
--- newapp/src/math.js
+++ newapp/src/math.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { App3 } from './App';
-
 export class Math extends React.Component {
     render() {
         return <App3 secondName = "Lis"/>;
