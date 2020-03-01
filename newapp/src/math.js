b44b0e01b584939464cea4333635d0ef8ca0639d
bad-news-everyone <miss.budnik@list.ru>
2020-03-01 17:41:41 +0300

    kk

---
+++ newapp/src/math.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { App } from './number.js';
+import { myNum } from './number.js';
+
+export const sum = myNum * 2;
+
+/*export const App2 = ({ number }) => {
+    const newNumber = number * 2;
+    return <App3 number = { newNumber }/> 
+};*/
---
+++ newapp/src/number.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const myNum = 8;
+
+/*export const App = () => {
+    return <App2 number={10} />
+};
+*/
